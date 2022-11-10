a7d6b69cf5fcb6e44798a80b7d46565404a23112
Sean <sean@mybeda.com>
2022-11-09 21:12:52 -0700

    parsed chances

--- huntResults/huntResults.py
+++ huntResults/huntResults.py
@@ -14,9 +14,14 @@ def parse_draw_results_by_line(result_lines):
     parsed_log = {}
     parsed_logs = []
     for i in result_lines:
-        parsed_log['hunt_area'] = re.search('(^\d{3})', i).group(0)
-        parsed_log['hunt_type'] = re.search('^\d{3}\s{2,}(\d{1,})\s{2,}', i).group(1)
-        parsed_log['description'] = re.search('\s{2,}([A-Z].+?)\s{2,}', i).group(1)
+        parsed_items = re.search('^(\d{3})\s{2,}(\d)\s{2,}(\w.*?)\s{2,}(\d)\s{2,}(\d{2,})\s{2,}(\d)\s{2,}(\d)', i)
+        parsed_log['hunt_area'] = parsed_items.group(1)
+        parsed_log['hunt_type'] = parsed_items.group(2)
+        parsed_log['description'] = parsed_items.group(3).lower()
+        parsed_log['total_quota'] = parsed_items.group(4)
+        parsed_log['first_choice_applicants'] = parsed_items.group(5)
+        parsed_log['second_choice_applicants'] = parsed_items.group(6)
+        parsed_log['third_choice_applicants'] = parsed_items.group(7)
         parsed_logs.append(parsed_log.copy())
 
     return parsed_logs
